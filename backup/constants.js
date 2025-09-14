b995a11747e75b26d98a5cdd389f47a65b3827c3
M-Majed <106390240+M-Majed@users.noreply.github.com>
2025-09-14 22:26:42 +0330

    cleaning

--- backup/constants.js
+++
@@ -1,8 +0,0 @@
-import Marketplace from './Marketplace.json';
-import NFT from './NFT.json';
-
-export const MarketplaceAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-export const NFTAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-
-export const MarketplaceABI = Marketplace.abi;
-export const NFTABI = NFT.abi;
